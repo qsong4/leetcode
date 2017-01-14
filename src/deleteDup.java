70807082a886dae17c55b472ee99b90f8409bd47
QingYuan Song <qsong4@hawk.iit.edu>
2017-01-14 10:01:44 -0600

    delete duplication

---
+++ src/deleteDup.java
@@ -0,0 +1,35 @@
+/**
+ * Created by songqingyuan on 1/14/17.
+ */
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode(int x){val = x;}
+}
+public class deleteDup {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode node = head;
+        if(node==null){
+            return head;
+        }
+
+        if(node.next==null){
+            return head;
+        }
+
+        while(node.next!=null){
+            ListNode tem = node.next;
+            if(node.next.val==node.val){
+                node.next = tem.next;
+            }else{
+                node = node.next;
+            }
+
+            if(node==null){
+                break;
+            }
+        }
+
+        return head;
+    }
+}
