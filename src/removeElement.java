a55fec9f64d91c611b5a254aaadb198398c16b35
QingYuan Song <qsong4@hawk.iit.edu>
2017-01-17 17:58:45 -0600

    removeElement

--- src/removeElement.java
+++ src/removeElement.java
@@ -3,7 +3,7 @@
  */
 public class removeElement {
     public int removeElement(int[] nums, int val) {
-        int count = 0;
+        int count = 1;
         for(int i=0;i<nums.length;i++){
             if(nums[i] != val){
                 nums[count++] = nums[i];
