33919bd2c98f5a32e2150b4aafd626d9e0fa52b6
QingYuan Song <qsong4@hawk.iit.edu>
2017-01-16 21:42:21 -0600

    remove element

---
+++ src/removeElement.java
@@ -0,0 +1,14 @@
+/**
+ * Created by songqingyuan on 1/16/17.
+ */
+public class removeElement {
+    public int removeElement(int[] nums, int val) {
+        int count = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] != val){
+                nums[count++] = nums[i];
+            }
+        }
+        return count;
+    }
+}
