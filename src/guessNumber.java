a6b1e10a8fb8013bb5c02a48ae93eec1782828da
QingYuan Song <qsong4@hawk.iit.edu>
2017-01-23 16:30:14 -0600

    find anagram

--- src/guessNumber.java
+++ src/guessNumber.java
@@ -1,24 +1,24 @@
 /**
  * Created by songqingyuan on 1/23/17.
  */
-public class guessNumber {
-    public int guessNumber(int n) {
-        int i=1;
-        int j=n;
-
-
-        while(i<j){
-            int mid =i + (j-i)/2;
-            if(guess(mid)==0){
-                return mid;
-            }
-            else if(guess(mid)==1){
-                i = mid+1;
-            }
-            else if(guess(mid)==-1){
-                j = mid;
-            }
-        }
-        return i;
-    }
-}
+//public class guessNumber {
+//    public int guessNumber(int n) {
+//        int i=1;
+//        int j=n;
+//
+//
+//        while(i<j){
+//            int mid =i + (j-i)/2;
+//            if(guess(mid)==0){
+//                return mid;
+//            }
+//            else if(guess(mid)==1){
+//                i = mid+1;
+//            }
+//            else if(guess(mid)==-1){
+//                j = mid;
+//            }
+//        }
+//        return i;
+//    }
+//}
