21b72ba3b8584aad63be447b1fd4fb2bfbccb3b7
QingYuan Song <qsong4@hawk.iit.edu>
2017-01-24 10:22:37 -0600

    hashPathSum

---
+++ src/hashPathSum.java
@@ -0,0 +1,10 @@
+/**
+ * Created by songqingyuan on 1/24/17.
+ */
+public class hashPathSum {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root==null) return false;
+        if(root.left==null&&root.right==null&&root.val == sum) return true;
+        return (hasPathSum(root.left, sum-root.left.val)|| hasPathSum(root.right, sum-root.right.val));
+    }
+}
