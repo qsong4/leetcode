899579fc98f3e6ae0944a0a6037fd42ad78be98e
QingYuan Song <qsong4@hawk.iit.edu>
2017-01-30 10:15:05 -0600

    llongest common prefix

---
+++ src/longestCommonPre.java
@@ -0,0 +1,17 @@
+/**
+ * Created by songqingyuan on 1/30/17.
+ */
+public class longestCommonPre {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs==null||strs.length==0) return "";
+        String pre = strs[0];
+        int i = 1;
+        while(i<strs.length){
+            while(strs[i].indexOf(pre)!=0){
+                pre = pre.substring(0, pre.length()-1);
+            }
+            i++;
+        }
+        return pre;
+    }
+}
