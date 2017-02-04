7dae173760da8034b07bb1282ae9518272cd080f
QingYuan Song <qsong4@hawk.iit.edu>
2017-02-04 10:47:22 -0600

    first bad Version

---
+++ src/firstBadVersion.java
@@ -0,0 +1,19 @@
+/**
+ * Created by songqingyuan on 2/4/17.
+ */
+public class firstBadVersion {
+
+    public boolean isBadVersion(int n){
+        return true;
+    }
+
+    public int firstBadVersion(int n) {
+        int start = 1, end = n;
+        while (start < end) {
+            int mid = start + (end-start) / 2;
+            if (!isBadVersion(mid)) start = mid + 1;
+            else end = mid;
+        }
+        return start;
+    }
+}
