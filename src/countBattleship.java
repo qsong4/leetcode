212b2572c2e22306f76c22fe1114cb73b1a6b1d8
QingYuan Song <qsong4@hawk.iit.edu>
2017-02-05 14:30:37 -0600

    count battleship

---
+++ src/countBattleship.java
@@ -0,0 +1,17 @@
+/**
+ * Created by songqingyuan on 2/5/17.
+ */
+public class countBattleship {
+    public int countBattleships(char[][] board) {
+        int count=0;
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[i].length;j++){
+                if(board[i][j]=='.') continue;
+                if(i>0&&board[i-1][j]=='X') continue;
+                if(j>0&&board[i][j-1]=='X') continue;
+                count++;
+            }
+        }
+        return count;
+    }
+}
