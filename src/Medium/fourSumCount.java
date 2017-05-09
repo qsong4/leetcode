a81edfdb876dc2e4d2cedeba2aacf7aa24bf4630
QingYuan Song <qsong4@hawk.iit.edu>
2017-05-08 22:46:05 -0500

    four sum count

---
+++ src/Medium/fourSumCount.java
@@ -0,0 +1,29 @@
+package Medium;
+import java.util.*;
+
+/**
+ * Created by songqingyuan on 5/8/17.
+ */
+public class fourSumCount {
+    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+
+        for(int i=0;i<C.length;i++){
+            for(int j=0;j<D.length;j++){
+                int sum = C[i] + D[j];
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
+            }
+        }
+
+        int res=0;
+
+        for(int i=0;i<A.length;i++){
+            for(int j=0;j<B.length;j++){
+                int sum = A[i] + B[j];
+                res = res + map.getOrDefault(-1*sum, 0);
+            }
+        }
+
+        return res;
+    }
+}
