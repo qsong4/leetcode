ea230b92b350c47860f9f23a3f63baff44d5f519
QingYuan Song <qsong4@hawk.iit.edu>
2017-05-17 16:24:47 -0500

    kth small BST

---
+++ src/Medium/kthSmall.java
@@ -0,0 +1,23 @@
+package Medium;
+
+/**
+ * Created by songqingyuan on 5/17/17.
+ */
+public class kthSmall {
+    public int kthSmallest(TreeNode root, int k) {
+        int count = countNodes(root.left);
+        if (k <= count) {
+            return kthSmallest(root.left, k);
+        } else if (k > count + 1) {
+            return kthSmallest(root.right, k-1-count); // 1 is counted as current node
+        }
+
+        return root.val;
+    }
+
+    public int countNodes(TreeNode n) {
+        if (n == null) return 0;
+
+        return 1 + countNodes(n.left) + countNodes(n.right);
+    }
+}
